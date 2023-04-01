8c08986dd11797cab75870fcab49d497564d162c
getssh <gtamrat33@gmail.com>
2023-04-01 22:17:03 +0300

    Add router links to header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Book from './components/Book';
 import BookCatagory from './components/bookCatagory/BookCatagory';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route index element={<Book />} />
           <Route path="categories" element={<BookCatagory />} />
