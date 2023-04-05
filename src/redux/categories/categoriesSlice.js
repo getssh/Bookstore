5a40204015dfdc704c662aeebc42b4128559838b
getssh <gtamrat33@gmail.com>
2023-04-05 12:00:56 +0300

    Add slice for book catagory

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {categories: []};
+
+const catagorySlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {
+    checkStatus: (state) => {
+      state.categories = "Under construction"
+    }
+  }
+})
+
+export const {checkStatus} = catagorySlice.actions;
+export default catagorySlice.reducer;
