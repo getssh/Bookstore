0036f67e387e5a6f7629d18e27d3387b55bde563
getssh <gtamrat33@gmail.com>
2023-04-05 11:41:13 +0300

    Add bookSlice in booksSlice.js

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit';
+import uuid from 'react-uuid';
+const initialState = {books: []}
+
+const bookSlice = createSlice({
+  name: "books",
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push({bookId: uuid() , name: action.payload.name, author: action.payload.author})
+    },
+    removeBook: (state, action) => {
+      state.books.filter((book) => book.id !== action.payload.id)
+    }
+  }
+})
+
+export const {addBook, removeBook} = bookSlice.actions;
+export default bookSlice.reducer;
