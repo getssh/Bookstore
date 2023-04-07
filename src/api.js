3d813f93ccbcdfd6c6a7976a0a9d76d06832b608
getssh <gtamrat33@gmail.com>
2023-04-07 23:43:08 +0300

    Add post method to create book

--- src/api.js
+++ src/api.js
@@ -2,9 +2,17 @@ import axios from 'axios';
 
 const apiKey = 'u4wOJkEcCFB44WNH7OIZ';
 const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${apiKey}/books/`;
-const getBook = async () => {
-  const response = axios.get(url);
-  return response;
+export const getBook = async () => {
+  const response = await axios.get(url);
+  return response.data;
 };
 
-export default getBook;
+export const createBook = async (book) => {
+  const response = await axios.post(url, book);
+  return response.data;
+};
+
+export const deleteBook = async (id) => {
+  const response = await axios.delete(url, id);
+  return response;
+};
