9d961b3de88dc6bd064568127ed3a3715aaaa833
getssh <gtamrat33@gmail.com>
2023-04-07 21:17:01 +0300

    Render books from api by using useEffect

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,49 @@
 import './Book.css';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookInfo from './bookInfo/BookInfo';
 import SingleBook from './SingleBook/SingleBook';
+import { getBooks } from '../redux/books/booksSlice';
 
 const Book = () => {
-  const books = useSelector((state) => state.books.books);
+  const { books, isLoading, error } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks);
+  }, [dispatch]);
 
   return (
     <div>
       <h2>Book Store</h2>
-      <ul>
-        {books.map((book) => (
-          <div key={book.item_id}>
-            <SingleBook
-              bookName={book.title}
-              authorName={book.author}
-              id={book.item_id}
-            />
-          </div>
-        ))}
-      </ul>
+      {
+        isLoading
+        && (
+        <div>
+          <h2>Loading...</h2>
+        </div>
+        )
+      }
+      {
+        error
+        && (
+        <div>
+          <h3>Error...</h3>
+        </div>
+        )
+      }
+      <div>
+        <ul>
+          {books.map((book) => (
+            <div key={book.item_id}>
+              <SingleBook
+                bookName={book.title}
+                authorName={book.author}
+                id={book.item_id}
+              />
+            </div>
+          ))}
+        </ul>
+      </div>
       <BookInfo />
     </div>
   );
